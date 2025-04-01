834225b68b9420c58c57b5dcecb2f533884079a7
SERGEY <96051914+Sergeiphantom86@users.noreply.github.com>
2025-04-01 12:34:29 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,44 @@
 class Player
 {
+    public int _age;
+
+    public Player(string name, int age)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Имя не может быть пустым", nameof(name));
+
+        if (age < 0)
+            throw new ArgumentOutOfRangeException(nameof(age), "Возраст не может быть отрицательным");
+
+        _age = age;
+
+    }
+
     public string Name { get; private set; }
-    public int Age { get; private set; }
 }
 
 class Weapon
 {
-    public float WeaponCooldown { get; private set; }
-    public int WeaponDamage { get; private set; }
+    private int _damage;
+    public float _cooldown { get; private set; }
+
+    public Weapon(int damage, float cooldown)
+    {
+        if (damage < 0)
+            throw new ArgumentOutOfRangeException(nameof(damage), "Урон не может быть отрицательным");
+
+        if (cooldown < 0)
+            throw new ArgumentOutOfRangeException(nameof(cooldown), "Время перезарядки не может быть отрицательным");
+
+        _damage = damage;
+        _cooldown = cooldown;
+    }
 
     public void Attack()
     {
-        
+
     }
 
     public bool IsReloading()
@@ -22,12 +49,21 @@ class Weapon
 
 class Mover
 {
+    private float _speed;
+
+    public Mover(float speed)
+    {
+        if (speed < 0)
+            throw new ArgumentOutOfRangeException(nameof(speed), "Скорость не может быть отрицательной");
+
+        _speed = speed;
+    }
+
     public float MovementDirectionX { get; private set; }
     public float MovementDirectionY { get; private set; }
-    public float MovementSpeed { get; private set; }
 
     public void Move()
     {
-        
+
     }
 }
